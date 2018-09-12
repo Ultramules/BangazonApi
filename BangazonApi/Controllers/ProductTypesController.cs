152eee1890ae631111cd4701d529dbdfc102fdc4
element2562 <element25668@gmail.com>
2018-09-12 15:16:07 -0500

    File structure created

---
+++ BangazonApi/Controllers/ProductTypesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstSprint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypesController : ControllerBase
+    {
+        // GET: api/ProductTypes
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ProductTypes/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ProductTypes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ProductTypes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

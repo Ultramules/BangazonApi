5674e127b93751fb6a70b7b8f7fba1db2191a458
Michael Roberts <35013526+MDR88@users.noreply.github.com>
2018-09-13 11:44:48 -0500

    Moved the two SQL files to my current branch with the latest master. There have not been any changes to these files.

---
+++ BangazonApi/Bangazon_Drop_Delete_Queries.sql
@@ -0,0 +1,46 @@
+USE Bangazon;
+
+DELETE FROM Departments;
+DELETE FROM Employees;
+DELETE FROM Computers;
+DELETE FROM TrainingPrograms;
+DELETE FROM EmployeeComputers;
+DELETE FROM EmployeeTrainingRegiments;
+DELETE FROM Customers;
+DELETE FROM ProductTypes;
+DELETE FROM Products;
+DELETE FROM Payments;
+DELETE FROM Orders;
+DELETE FROM ProductOrders;
+
+
+
+
+ALTER TABLE Employees DROP CONSTRAINT [FK_Department_Employees];
+ALTER TABLE EmployeeComputers DROP CONSTRAINT [FK_EmployeeComputer];
+ALTER TABLE EmployeeComputers DROP CONSTRAINT [FK_ComputerEmployee];
+ALTER TABLE EmployeeTrainingRegiments DROP CONSTRAINT [FK_EmployeeTraining];
+ALTER TABLE EmployeeTrainingRegiments DROP CONSTRAINT [FK_TrainingEmployees];
+ALTER TABLE Products DROP CONSTRAINT [FK_ProductTypeId];
+ALTER TABLE Products DROP CONSTRAINT [FK_CustomerId];
+ALTER TABLE Payments DROP CONSTRAINT [FK_CustomerPayment];
+ALTER TABLE Orders DROP CONSTRAINT [FK_PaymentId];
+ALTER TABLE Orders DROP CONSTRAINT [FK_CustomerOrders];
+ALTER TABLE ProductOrders DROP CONSTRAINT [FK_OrderId];
+ALTER TABLE ProductOrders DROP CONSTRAINT [FK_ProductId];
+
+
+
+DROP TABLE IF EXISTS Departments;
+DROP TABLE IF EXISTS Employees;
+DROP TABLE IF EXISTS Computers;
+DROP TABLE IF EXISTS TrainingPrograms;
+DROP TABLE IF EXISTS EmployeeComputers;
+DROP TABLE IF EXISTS EmployeeTrainingRegiments;
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS ProductTypes;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Payments;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS ProductOrders;
+

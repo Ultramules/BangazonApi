e3902fe99204b199a0c0c40391597a8f9589d703
Adelaide <adelaide.yoder@gmail.com>
2018-09-14 08:01:14 -0700

    created the employees model

--- BangazonApi/Models/Employees.cs
+++ BangazonApi/Models/Employees.cs
@@ -7,6 +7,12 @@ namespace firstSprint.Models
 {
     public class Employees
     {
-
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Supervisor { get; set; }
+        public Departments Department { get; set; }
     }
 }
+
+
